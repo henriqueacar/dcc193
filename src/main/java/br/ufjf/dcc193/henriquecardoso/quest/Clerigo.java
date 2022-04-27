6466c67485d71df97f4991090bcf568906341f59
Henrique Cardoso <henrique_acar@hotmail.com>
2022-04-26 22:29:15 -0300

    Adiciona classe Clerigo e factory

---
+++ src/main/java/br/ufjf/dcc193/henriquecardoso/quest/Clerigo.java
@@ -0,0 +1,11 @@
+package br.ufjf.dcc193.henriquecardoso.quest;
+
+public class Clerigo implements Heroi {
+    public Clerigo(){
+        System.out.println("Um novo clerigo chega ao reino...");
+    }
+
+    public void agir(){
+        System.out.println("O clérigo cura!");
+    }
+}
---
+++ src/main/java/br/ufjf/dcc193/henriquecardoso/quest/Guilda.java
@@ -0,0 +1,7 @@
+package br.ufjf.dcc193.henriquecardoso.quest;
+
+public class Guilda {
+    public static Heroi createHeroi(){
+        return new Clerigo();
+    }
+}
