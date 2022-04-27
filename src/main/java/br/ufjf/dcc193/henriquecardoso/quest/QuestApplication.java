d365bb317950b3d279ae822c61e7d2279d4532a3
Henrique Cardoso <henrique_acar@hotmail.com>
2022-04-26 22:44:49 -0300

    Refatora paa uso do contexto spring

--- src/main/java/br/ufjf/dcc193/henriquecardoso/quest/QuestApplication.java
+++ src/main/java/br/ufjf/dcc193/henriquecardoso/quest/QuestApplication.java
@@ -2,19 +2,19 @@ package br.ufjf.dcc193.henriquecardoso.quest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class QuestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuestApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(QuestApplication.class, args);
 		System.out.println("Hora da Aventura");
 
-		Heroi h1 = Guilda.createHeroi();
+		Heroi h1 = ctx.getBean(Cavaleiro.class);
 		
-		Missao m1 = new Missao();
+		Missao m1 = ctx.getBean(Missao.class);
 		m1.iniciar();
-		m1.setHeroi(h1);
 		m1.concluir();
 	}
 
