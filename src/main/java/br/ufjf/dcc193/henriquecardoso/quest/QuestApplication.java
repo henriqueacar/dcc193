c3f529e836256608778aedca4c838728955a114e
Henrique Cardoso <henrique_acar@hotmail.com>
2022-04-26 21:54:36 -0300

    inicia um projeto spring boot

---
+++ src/main/java/br/ufjf/dcc193/henriquecardoso/quest/QuestApplication.java
@@ -0,0 +1,14 @@
+package br.ufjf.dcc193.henriquecardoso.quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestApplication.class, args);
+
+	}
+
+}
